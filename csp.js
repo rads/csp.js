18163c55f27e7d1841f290ba13d6d4d374d1c39f
Radford Smith <contact@radsmith.com>
2013-11-24 16:35:47 -0500

    Bump to version 0.2.4

--- csp.js
+++ csp.js
@@ -1,8 +1,8 @@
 /*
-* CSP.js 0.2.3
-* Copyright (c) 2013, Radford Smith
-* This code is released under the MIT license.
-*/
+ * CSP.js v0.2.4
+ * Copyright (c) 2013, Radford Smith
+ * This code is released under the MIT license.
+ */
 !function(e){"object"==typeof exports?module.exports=e():"function"==typeof define&&define.amd?define(e):"undefined"!=typeof window?window.CSP=e():"undefined"!=typeof global?global.CSP=e():"undefined"!=typeof self&&(self.CSP=e())}(function(){var define,module,exports;
 return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
@@ -1614,6 +1614,8 @@ function runMachine(machine, yieldVal) {
     chans.putAsync(machine.ret, retValue, function() {
       machine.ret.close();
     });
+  } else {
+    machine.ret.close();
   }
 }
 
